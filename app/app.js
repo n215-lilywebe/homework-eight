5e20a07da0ad09429dd4da3ec9f0df8f915fec80
lilywebe <55253338+lilywebe@users.noreply.github.com>
2022-03-30 22:12:52 -0400

    it is criminal how often i forget about github

---
+++ app/app.js
@@ -0,0 +1,220 @@
+var homeContent = `            <div class="hero">
+<h1>Candid Coffee Company</h1>
+</div>
+<div class="content">
+<div class="who-we-are">
+    <h1>Who We Are</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis facere, odit eligendi
+        aspernatur natus officiis asperiores amet sit et minus repudiandae cupiditate reprehenderit nam
+        quod maiores vel qui iste fugiat perspiciatis minima autem quasi? Doloribus iste magni eius
+        saepe, expedita id blanditiis commodi, dignissimos ut quia est sint perferendis tenetur, ad
+        molestiae nobis repellendus nisi officia doloremque numquam totam ullam non consequuntur
+        voluptatem! Corrupti et provident fuga ad odio, enim vel, corporis aliquid animi illum quod
+        voluptatibus unde qui architecto commodi vitae minima debitis dolore accusantium repellendus
+        voluptatem ipsa totam ut. Asperiores cumque perspiciatis rem, quam laboriosam eveniet quidem
+        quas.</p>
+</div>
+<div class="current-specials">
+    <div class="special">
+        <h1>Special</h1>
+        <img src="images/coffees/americano.png" alt="">
+    </div>
+    <div class="special">
+        <h1>Special</h1>
+        <img src="images/coffees/breve.png" alt="">
+    </div>
+    <div class="special">
+        <h1>Special</h1>
+        <img src="images/coffees/cappucino.png" alt="">
+    </div>
+</div>
+<div class="come-visit">
+    <div class="location-image">
+        <img src="images/coffee-shop.jpg" alt="">
+    </div>
+    <div class="location-desc">
+        <h1>Come Visit Us</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa cumque sint tempore fugiat
+            ipsum ex expedita quos commodi hic sit?</p>
+    </div>
+</div>
+<div class="upcoming-events">
+    <div class="event">
+        <h1>Event</h1>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Possimus similique fugit labore
+            iusto ea aperiam?</p>
+    </div>
+    <div class="event">
+        <h1>Event</h1>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Possimus similique fugit labore
+            iusto ea aperiam?</p>
+    </div>
+    <div class="event">
+        <h1>Event</h1>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Possimus similique fugit labore
+            iusto ea aperiam?</p>
+    </div>
+</div>
+
+</div>`;
+var aboutContent = `            <div class="hero">
+<h1>About Us</h1>
+</div>
+<div class="content">
+<div class="founder">
+    <div class="founder-image">
+        <img src="images/founder.jpg" alt="">
+    </div>
+    <div class="founder-desc">
+        <h1>Our Founder</h1>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat, quam! Molestias fugit
+        delectus velit soluta ullam rem magni. Reprehenderit vitae culpa, rem ex officia voluptate
+        temporibus animi consequatur odio aliquam?
+    </div>
+</div>
+<div class="our-mission">
+    <h1>Our Mission</h1>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quidem iste nemo adipisci error ad eum
+        pariatur beatae, ea rem ab itaque obcaecati et quasi reiciendis cupiditate est, dicta
+        voluptatibus repellat deleniti non dolor enim nam quo! Ad perferendis accusantium similique
+        suscipit mollitia aliquid, earum aspernatur cumque doloremque! Iusto, suscipit quis? Quaerat
+        atque quo consequuntur esse, nihil iure aliquid necessitatibus voluptatibus.</p>
+</div>
+<div class="community">
+    <div class="community-desc">
+        <h1>Our Community</h1>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cupiditate beatae, magnam facere
+            aspernatur iste laudantium accusantium, delectus nesciunt pariatur obcaecati quos cumque
+            autem qui similique nam, dignissimos aperiam ipsam iure.</p>
+    </div>
+    <div class="community-image">
+        <img src="images/community.jpg" alt="">
+    </div>
+</div>
+</div>`;
+var productsContent = `<div class="hero">
+<h1>Our Products</h1>
+</div>
+<div class="content">
+<div class="products">
+    <div class="product">
+        <div class="prod-image">
+            <img src="images/coffees/americano.png" alt="">
+        </div>
+        <div class="prod-descript">
+            <h4>Big City</h4>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptas deleniti nisi aliquid
+                dignissimos labore eius ab ratione veritatis fuga hic sed rem dolores, nulla quis?</p>
+
+        </div>
+    </div>
+    <div class="product">
+        <div class="prod-image">
+            <img src="images/coffees/breve.png" alt="">
+        </div>
+        <div class="prod-descript">
+            <h4>Big City</h4>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptas deleniti nisi aliquid
+                dignissimos labore eius ab ratione veritatis fuga hic sed rem dolores, nulla quis?</p>
+
+        </div>
+    </div>
+    <div class="product">
+        <div class="prod-image">
+            <img src="images/coffees/cappucino.png" alt="">
+        </div>
+        <div class="prod-descript">
+            <h4>Big City</h4>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptas deleniti nisi aliquid
+                dignissimos labore eius ab ratione veritatis fuga hic sed rem dolores, nulla quis?</p>
+
+        </div>
+    </div>
+    <div class="product">
+        <div class="prod-image">
+            <img src="images/coffees/espresso.png" alt="">
+        </div>
+        <div class="prod-descript">
+            <h4>Big City</h4>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptas deleniti nisi aliquid
+                dignissimos labore eius ab ratione veritatis fuga hic sed rem dolores, nulla quis?</p>
+
+        </div>
+    </div>
+    <div class="product">
+        <div class="prod-image">
+            <img src="images/coffees/espressomacchiato.png" alt="">
+        </div>
+        <div class="prod-descript">
+            <h4>Big City</h4>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptas deleniti nisi aliquid
+                dignissimos labore eius ab ratione veritatis fuga hic sed rem dolores, nulla quis?</p>
+
+        </div>
+    </div>
+</div>
+</div>`;
+
+var contactContent = `            <div class="hero">
+    <h1> Get In Touch!</h1>
+</div >
+    <div class="content">
+        <div class="split-screen">
+            <div class="location">
+                <div class="map">
+                    <img src="images/map.webp" alt="">
+                </div>
+                <div class="address">
+                    <h1>Our Address</h1>
+                    <p>5555 Main St <br> Indianapolis, IN 46217</p>
+
+                </div>
+            </div>
+            <div class="contact-methods">
+                <h1>Ways to Reach Us</h1>
+                <div class="phone">
+                    Phone: 555-555-5555
+                </div>
+                <div class="email">
+                    Email: admin@candidcoffeeco.com
+                </div>
+                <div class="mailbox">
+                    Mailbox: P.O. Box 1444
+                </div>
+            </div>
+        </div>
+        <div class="job-app">
+            <h1>Apply to Join Our Team</h1>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nesciunt laudantium earum repellat
+                consequuntur veritatis molestiae deleniti magnam! Totam ad vitae hic delectus impedit?
+                Distinctio sit dolorum, quae quibusdam autem iusto!</p>
+            <a href="#">Apply here!</a>
+        </div>
+    </div>`;
+
+function initListeners() {
+    $("nav a").click(function (e) {
+        let btnID = e.currentTarget.id;
+        loadPageContent(btnID);
+        //console.log(contentID);
+    })
+}
+
+function loadPageContent(pageName) {
+    let contentID = pageName + "Content";
+    $("#app").html(eval(contentID));
+}
+
+
+function initApp() {
+    initListeners();
+
+}
+
+
+
+$(document).ready(function () {
+    loadPageContent("home");
+    initApp();
+
+})
